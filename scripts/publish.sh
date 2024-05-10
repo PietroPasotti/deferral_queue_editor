46e7cb7f7a1a55f56657d61300c28b7709ae2ca8
Pietro Pasotti <starfire.daemon@gmail.com>
2024-05-10 15:07:41 +0200

    init template

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 LIB_V=${LIB_VERSION:-v0}
 echo "publishing lib..."
-charmcraft publish-lib "charms.deferral-queue-editor.$LIB_V.editor.py"  # $ TEMPLATE: Filled in by ./scripts/init.sh
+charmcraft publish-lib "charms.deferral-queue-editor.$LIB_V.editor"  # $ TEMPLATE: Filled in by ./scripts/init.sh
