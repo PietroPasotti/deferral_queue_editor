c4f93926153b53bee2dc6f7f7113143beaa76556
Pietro Pasotti <starfire.daemon@gmail.com>
2024-05-10 18:01:08 +0200

    working prototype

--- editor.py
+++ editor.py
@@ -1,1 +1,113 @@
-print(1)
+import dataclasses
+import re
+from contextlib import contextmanager
+from pathlib import Path
+from typing import Dict, List, Callable
+
+import ops
+from ops import CharmBase
+from ops.framework import _event_regex
+from ops.storage import SQLiteStorage, NoSnapshotError
+
+EVENT_REGEX = re.compile(_event_regex)
+
+
+@dataclasses.dataclass(frozen=True)
+class DeferredEvent:
+    handle_path: str
+    owner: str
+    observer: str
+
+    # needs to be marshal.dumps-able.
+    snapshot_data: Dict = dataclasses.field(default_factory=dict)
+
+    @property
+    def name(self):
+        return self.handle_path.split("/")[-1].split("[")[0]
+
+
+class _UnitStateDB:
+    def __init__(self, db: SQLiteStorage):
+        self._db = db
+
+    def read(self) -> List["DeferredEvent"]:
+        """Load the list of deferred events from the db."""
+
+        db = self._db
+
+        deferred = []
+        for handle_path in db.list_snapshots():
+            if EVENT_REGEX.match(handle_path):
+                notices = db.notices(handle_path)
+                for handle, owner, observer in notices:
+                    try:
+                        snapshot_data = db.load_snapshot(handle)
+                    except NoSnapshotError:
+                        snapshot_data = {}
+
+                    event = DeferredEvent(
+                        handle_path=handle,
+                        owner=owner,
+                        observer=observer,
+                        snapshot_data=snapshot_data,
+                    )
+                    deferred.append(event)
+
+        return deferred
+
+    def write(self, deferred: List[DeferredEvent], replace=True):
+        """Write the deferred events to the queue.
+
+        If ``replace``: clear the existing queue first.
+        """
+        db = self._db
+        if replace:
+            for handle_path in db.list_snapshots():
+                if EVENT_REGEX.match(handle_path):
+                    notices = db.notices(handle_path)
+                    for notice in notices:
+                        db.drop_notice(*notice)
+
+        for obj in deferred:
+            db.save_notice(obj.handle_path, obj.owner, obj.observer)
+            db.save_snapshot(obj.handle_path, obj.snapshot_data)
+
+
+def deferred(event_name: str,
+             handler: Callable,
+             event_id: int = 1,
+             snapshot_data: Dict[str, str] = None) -> DeferredEvent:
+    """Helper to create a deferred event data structure from an event name and its designated handler.
+    Note that different event types will require you to pass specific snapshot_data pairs.
+    Refer to the ops docs for the full specification.
+    """
+    handler_repr = repr(handler)
+    handler_re = re.compile(r"<function (.*) at .*>")
+    boundmethod_re = re.compile(r"<bound method (.*) of .*>")
+    match = handler_re.match(handler_repr) or boundmethod_re.match(handler_repr)
+    if not match:
+        raise ValueError(
+            f"cannot construct DeferredEvent from {handler}; please create one manually.",
+        )
+    owner_name, handler_name = match.groups()[0].split(".")[-2:]
+    handle_path = f"{owner_name}/on/{event_name}[{event_id}]"
+
+    return DeferredEvent(
+        handle_path,
+        owner_name,
+        handler_name,
+        snapshot_data=snapshot_data,
+    )
+
+
+@contextmanager
+def edit_deferral_queue(charm: ops.CharmBase):
+    db = _UnitStateDB(charm.framework._storage)
+    queue = db.read()
+    yield queue
+    db.write(queue, replace=True)
+
+
+def get_deferral_queue(charm: ops.CharmBase):
+    db = _UnitStateDB(charm.framework._storage)
+    return db.read()
--- test/test_unit.py
+++ test/test_unit.py
@@ -1,5 +1,55 @@
 # add here your unittests
+import pytest
+from ops import CharmBase, Framework
+from scenario import Context, State
+
+from editor import deferred, edit_deferral_queue
 
 
-def test_foo():
-    print(1)
+class MyCharm(CharmBase):
+    def __init__(self, framework: Framework):
+        super().__init__(framework)
+
+        with edit_deferral_queue(self) as queue:
+            queue.pop(0)
+            queue.extend([
+                deferred("update_status", self._on_event),
+                deferred("start", self._on_event),
+                deferred("install", self._on_event),
+            ])
+            queue.sort(key=lambda x: x.name)
+
+        for e in self.on.events().values():
+            framework.observe(e, self._on_event)
+
+    def _on_event(self, e):
+        pass
+
+
+@pytest.fixture
+def ctx():
+    return Context(
+        MyCharm,
+        meta={"name": "mehdi"},
+        capture_deferred_events=True,
+    )
+
+
+def test_editor(ctx):
+    ctx.run("update-status", State(
+            deferred=[
+                deferred("stop", ctx.charm_spec.charm_type._on_event),
+                deferred("remove", ctx.charm_spec.charm_type._on_event),
+                deferred("upgrade_charm", ctx.charm_spec.charm_type._on_event),
+            ]
+    ))
+    def _to_name(e):
+        pth = getattr(e, "handle_path", e.handle.path)
+        return pth.split("/")[-1].split('[')[0]
+
+    assert [_to_name(e) for e in ctx.emitted_events] == ['install',
+     'start',
+     'stop',
+     'update_status',
+     'upgrade_charm',
+     'update_status']
